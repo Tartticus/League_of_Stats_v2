42b3fd0a6bb390869d4558bec8cb44e4d644502c
Matthew Tartt <87999009+Tartticus@users.noreply.github.com>
2025-04-01 19:06:52 -0700

    Update win_rate.sql

--- dbt/models/stats/win_rate.sql
+++ dbt/models/stats/win_rate.sql
@@ -3,4 +3,7 @@
     alias='Champ_Winrates'
 ) }}
 SELECT player_champ, (100*COUNT(CASE WHEN WIN = 'True' THEN 1 END)/COUNT(WIN)) As "win_rate%" , COUNT(WIN) as games_played FROM LEAGUEOFSTATS.MAIN.PLAYER_INFO P 
+INNER JOIN MATCHES M ON M.MATCH_ID = P.MATCH_ID
+
+WHERE M.GAME_MODE = 'Ranked'
 group by player_champ
